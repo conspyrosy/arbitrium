89e3a0dd477abfe94d105d950916f0fdf10f814e
andrekorol <anrobits@yahoo.com.br>
2021-01-19 18:06:21 -0300

    Add decimals as constants

--- constants/hegic.js
+++ constants/hegic.js
@@ -1,11 +1,13 @@
 const wbtcPool = {
     underlying: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
-    pool: '0x3961245db602ed7c03eeccda33ea3846bd8723bd'
+    pool: '0x3961245db602ed7c03eeccda33ea3846bd8723bd',
+    decimals: 8,
 }
 
 const ethPool = {
     underlying: '0x0000000000000000000000000000000000000000',
-    pool: '0xEfC0eEAdC1132A12c9487d800112693bf49EcfA2'
+    pool: '0xEfC0eEAdC1132A12c9487d800112693bf49EcfA2',
+    decimals: 18,
 }
 
 module.exports = {
